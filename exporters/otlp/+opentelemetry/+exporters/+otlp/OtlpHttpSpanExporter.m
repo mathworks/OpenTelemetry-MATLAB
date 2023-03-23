70039828852abee60d11dd86a6b685a3a8a6b00d
duncanpo <dpo@mathworks.com>
2023-03-23 14:29:46 -0400

    Added support for Http exporter options.

--- exporters/otlp/+opentelemetry/+exporters/+otlp/OtlpHttpSpanExporter.m
+++ exporters/otlp/+opentelemetry/+exporters/+otlp/OtlpHttpSpanExporter.m
@@ -11,7 +11,11 @@ classdef OtlpHttpSpanExporter
 
     properties (SetAccess=immutable)
         Endpoint (1,1) string
+        Format (1,1) string
+        JsonBytesMapping (1,1) string
+        UseJsonName (1,1) logical
         Timeout (1,1) duration
+        HttpHeaders (1,1) dictionary
     end
 
     methods
@@ -21,10 +25,16 @@ classdef OtlpHttpSpanExporter
                 optionvalues
             end
 
-            validnames = ["Endpoint", "Timeout"];
+            validnames = ["Endpoint", "Format", "JsonBytesMapping", ...
+                "UseJsonName", "Timeout", "HttpHeaders"];
             % set default values to empty or negative
             endpoint = "";
+            dataformat = "";
+            jsonbytesmapping = "";
+            usejsonname = false;
             timeout_millis = -1;
+            headerkeys = string.empty();
+            headervalues = string.empty();
             for i = 1:length(optionnames)
                 namei = validatestring(optionnames{i}, validnames);
                 valuei = optionvalues{i};
@@ -33,32 +43,70 @@ classdef OtlpHttpSpanExporter
                         error("Endpoint must be a scalar string.");
                     end
                     endpoint = string(valuei);
-                else   % "Timeout" 
+                elseif strcmp(namei, "Format")
+                    dataformat = validatestring(valuei, ["JSON", "binary"]);
+                elseif strcmp(namei, "JsonBytesMapping")
+                    jsonbytesmapping = validatestring(valuei, ["hex", "hexId", "base64"]);
+                elseif strcmp(namei, "UseJsonName")
+                    if ~((islogical(valuei) || isnumeric(valuei)) && isscalar(valuei))
+                        error("UseJsonName must be a scalar logical.")
+                    end
+                    usejsonname = logical(valuei);
+                elseif  strcmp(namei, "Timeout") 
                     if ~(isduration(valuei) && isscalar(valuei)) 
                         error("Timeout must be a scalar duration.");
                     end
                     timeout = valuei;
                     timeout_millis = milliseconds(timeout);
+                else  % HttpHeaders
+                    if ~isa(valuei, "dictionary")
+                        error("HttpHeaders input must be a dictionary.");
+                    end
+                    httpheaders = valuei;
+                    headerkeys = keys(valuei);
+                    headervalues = values(valuei);
+                    if ~isstring(headervalues)
+                        error("HttpHeaders dictionary values must be strings.")
+                    end
                 end
             end
             
             obj.Proxy = libmexclass.proxy.Proxy("Name", ...
                 "libmexclass.opentelemetry.exporters.OtlpHttpSpanExporterProxy", ...
-                "ConstructorArguments", {endpoint, timeout_millis});
+                "ConstructorArguments", {endpoint, dataformat, jsonbytesmapping, ...
+                usejsonname, timeout_millis, headerkeys, headervalues});
 
             % populate immutable properties
-            if endpoint == "" || timeout_millis < 0
-                [defaultendpoint, defaultmillis] = obj.Proxy.getDefaultOptionValues();
+            if endpoint == "" || dataformat == "" || jsonbytesmapping == "" || ...
+                    timeout_millis < 0
+                [defaultendpoint, defaultformat, defaultmapping, defaultmillis] = ...
+                    obj.Proxy.getDefaultOptionValues();
             end
             if endpoint == ""  % not specified, use default value
                 obj.Endpoint = defaultendpoint;
             else
                 obj.Endpoint = endpoint;
             end
+            if dataformat == ""  % not specified, use default value
+                obj.Format = defaultformat;
+            else
+                obj.Format = dataformat;
+            end
+            if jsonbytesmapping == ""  % not specified, use default value
+                obj.JsonBytesMapping = defaultmapping;
+            else
+                obj.JsonBytesMapping = jsonbytesmapping;
+            end
+            obj.UseJsonName = usejsonname;
             if timeout_millis < 0  % not specified, use default value
                 obj.Timeout = milliseconds(defaultmillis);
             else
                 obj.Timeout = timeout;
+            end
+            if isempty(headerkeys)  % not specified, return empty dictionary
+                obj.HttpHeaders = dictionary(headerkeys, headervalues);
+            else
+                obj.HttpHeaders = httpheaders;
             end
         end
     end
--- exporters/otlp/src/OtlpHttpSpanExporterProxy.cpp
+++ exporters/otlp/src/OtlpHttpSpanExporterProxy.cpp
@@ -12,14 +12,47 @@ namespace libmexclass::opentelemetry::exporters {
 OtlpHttpSpanExporterProxy::OtlpHttpSpanExporterProxy(const libmexclass::proxy::FunctionArguments& constructor_arguments) {
     matlab::data::StringArray endpoint_mda = constructor_arguments[0];
     std::string endpoint = static_cast<std::string>(endpoint_mda[0]);
-    matlab::data::TypedArray<double> timeout_mda = constructor_arguments[1];
+    matlab::data::StringArray dataformat_mda = constructor_arguments[1];
+    std::string dataformat = static_cast<std::string>(dataformat_mda[0]);
+    matlab::data::StringArray json_bytes_mapping_mda = constructor_arguments[2];
+    std::string json_bytes_mapping = static_cast<std::string>(json_bytes_mapping_mda[0]);
+    matlab::data::TypedArray<bool> use_json_name_mda = constructor_arguments[3];
+    bool use_json_name = use_json_name_mda[0];
+    matlab::data::TypedArray<double> timeout_mda = constructor_arguments[4];
     double timeout = timeout_mda[0];
+    matlab::data::Array header_mda = constructor_arguments[5];
+    size_t nheaders = header_mda.getNumberOfElements();
+    matlab::data::StringArray headernames_mda = constructor_arguments[5];
+    matlab::data::StringArray headervalues_mda = constructor_arguments[6];
 
     if (!endpoint.empty()) {
         CppOptions.url = endpoint;
     } 
+    // TODO: store the relationship between strings and enums in an associative container
+    // dataformat
+    if (dataformat.compare("JSON") == 0) {
+        CppOptions.content_type = otlp_exporter::HttpRequestContentType::kJson;
+    } else if (dataformat.compare("binary") == 0) {
+        CppOptions.content_type = otlp_exporter::HttpRequestContentType::kBinary;
+    }
+    // json_bytes_mapping
+    if (json_bytes_mapping.compare("hex") == 0) {
+        CppOptions.json_bytes_mapping = otlp_exporter::JsonBytesMappingKind::kHex;
+    } else if (json_bytes_mapping.compare("hexId") == 0) {
+        CppOptions.json_bytes_mapping = otlp_exporter::JsonBytesMappingKind::kHexId;
+    } else if (json_bytes_mapping.compare("base64") == 0) {
+        CppOptions.json_bytes_mapping = otlp_exporter::JsonBytesMappingKind::kBase64;
+    }
+    // use_json_name
+    CppOptions.use_json_name = use_json_name;
+    // timeout
     if (timeout >= 0) {
         CppOptions.timeout = std::chrono::milliseconds(static_cast<int64_t>(timeout));
+    }
+    // http headers
+    for (size_t i = 0; i < nheaders; ++i) {
+        CppOptions.http_headers.insert(std::pair{static_cast<std::string>(headernames_mda[i]),
+				static_cast<std::string>(headervalues_mda[i])});
     }
     REGISTER_METHOD(OtlpHttpSpanExporterProxy, getDefaultOptionValues);
 }
@@ -32,9 +65,28 @@ void OtlpHttpSpanExporterProxy::getDefaultOptionValues(libmexclass::proxy::metho
     otlp_exporter::OtlpHttpExporterOptions options;
     matlab::data::ArrayFactory factory;
     auto endpoint_mda = factory.createScalar(options.url);
+    std::string dataformat, json_bytes_mapping;
+    // dataformat
+    if (options.content_type == otlp_exporter::HttpRequestContentType::kJson) {
+        dataformat = "JSON";
+    } else {
+	dataformat = "binary";
+    }
+    // json_bytes_mapping
+    if (options.json_bytes_mapping == otlp_exporter::JsonBytesMappingKind::kHex) {
+        json_bytes_mapping = "hex";
+    } else if (options.json_bytes_mapping == otlp_exporter::JsonBytesMappingKind::kHexId) {
+        json_bytes_mapping = "hexId";
+    } else {   // kBase64
+        json_bytes_mapping = "base64";
+    }
+    auto dataformat_mda = factory.createScalar(dataformat);
+    auto json_bytes_mapping_mda = factory.createScalar(json_bytes_mapping);
     auto timeout_millis = std::chrono::duration_cast<std::chrono::milliseconds>(options.timeout);
     auto timeout_mda = factory.createScalar(static_cast<double>(timeout_millis.count()));
     context.outputs[0] = endpoint_mda;
-    context.outputs[1] = timeout_mda;
+    context.outputs[1] = dataformat_mda;
+    context.outputs[2] = json_bytes_mapping_mda;
+    context.outputs[3] = timeout_mda;
 }
 } // namespace libmexclass::opentelemetry
