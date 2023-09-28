98bd3a6a7750c258abbbb21d00a0b2f750fde63e
Rick Tu <ricktu@mathworks.com>
2023-09-28 13:36:59 -0400

    v2

--- exporters/otlp/include/opentelemetry-matlab/exporters/otlp/OtlpHttpMetricExporterProxy.h
+++ exporters/otlp/include/opentelemetry-matlab/exporters/otlp/OtlpHttpMetricExporterProxy.h
@@ -8,7 +8,11 @@
 #include "libmexclass/proxy/method/Context.h"
 
 #include "opentelemetry/sdk/metrics/push_metric_exporter.h"
+<<<<<<< Updated upstream
 #include "opentelemetry/exporters/otlp/otlp_http_exporter_options.h"
+=======
+#include "opentelemetry/exporters/otlp/otlp_http_metric_exporter_options.h"
+>>>>>>> Stashed changes
 
 namespace metric_sdk = opentelemetry::sdk::metrics;
 namespace otlp_exporter = opentelemetry::exporter::otlp;
@@ -16,8 +20,14 @@ namespace otlp_exporter = opentelemetry::exporter::otlp;
 namespace libmexclass::opentelemetry::exporters {
 class OtlpHttpMetricExporterProxy: public libmexclass::opentelemetry::sdk::MetricExporterProxy {
   public:
+<<<<<<< Updated upstream
     OtlpHttpMetricExporterProxy(otlp_exporter::OtlpHttpExporterOptions options) : CppOptions(options) {
         REGISTER_METHOD(OtlpHttpMetricExporterProxy, getDefaultOptionValues);
+=======
+    OtlpHttpMetricExporterProxy(otlp_exporter::OtlpHttpMetricExporterOptions options) : CppOptions(options) {
+        REGISTER_METHOD(OtlpHttpMetricExporterProxy, getDefaultOptionValues);
+        REGISTER_METHOD(OtlpHttpMetricExporterProxy, test);
+>>>>>>> Stashed changes
     }
 
     static libmexclass::proxy::MakeResult make(const libmexclass::proxy::FunctionArguments& constructor_arguments);
@@ -26,7 +36,14 @@ class OtlpHttpMetricExporterProxy: public libmexclass::opentelemetry::sdk::Metri
 
     void getDefaultOptionValues(libmexclass::proxy::method::Context& context);
 
+<<<<<<< Updated upstream
   private:
     otlp_exporter::OtlpHttpExporterOptions CppOptions;
+=======
+    void test(libmexclass::proxy::method::Context& context);
+
+  private:
+    otlp_exporter::OtlpHttpMetricExporterOptions CppOptions;
+>>>>>>> Stashed changes
 };
 } // namespace libmexclass::opentelemetry
