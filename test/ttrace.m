b55ffdfc42e5ecc8de8c62ae32bf9c41e26a381c
duncanpo <dpo@mathworks.com>
2023-10-25 12:23:37 -0400

    Fix a bug in span/setStatus #20

--- test/ttrace.m
+++ test/ttrace.m
@@ -262,6 +262,28 @@ classdef ttrace < matlab.unittest.TestCase
                 "convertFrom", "posixtime") - endtime), seconds(2));
         end
 
+        function testStatus(testCase)
+            % testStatus: setting status
+            tp = opentelemetry.sdk.trace.TracerProvider();
+            tr = getTracer(tp, "foo");
+            sp = startSpan(tr, "bar");
+            setStatus(sp, "ok");
+            endSpan(sp);
+
+            sp1 = startSpan(tr, "quz");
+            setStatus(sp1, "Error", "Something went wrong.")  % with description
+            endSpan(sp1);
+
+            % perform test comparisons
+            results = readJsonResults(testCase);
+            % status codes
+            %   Unset: 0
+            %   Ok:    1
+            %   Error: 2
+            verifyEqual(testCase, results{1}.resourceSpans.scopeSpans.spans.status.code, 1);
+            verifyEqual(testCase, results{2}.resourceSpans.scopeSpans.spans.status.code, 2);
+        end
+
         function testAttributes(testCase)
             % testAttributes: specifying attributes when starting spans
 
