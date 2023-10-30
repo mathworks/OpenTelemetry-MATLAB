e245e2e69544c2e51dc414f21c8594a1279e1306
Devin Narula <dnarula@mathworks.com>
2023-10-30 13:30:07 -0400

    Removing unnecessary change in tests

--- test/commonSetup.m
+++ test/commonSetup.m
@@ -4,5 +4,5 @@ function commonSetup(testCase)
 % Copyright 2023 The MathWorks, Inc.
 
 % start collector
-system(testCase.Otelcol + " --config " + '"' + testCase.OtelConfigFile + '"' + '&');
+system(testCase.Otelcol + " --config " + testCase.OtelConfigFile + '&');
 pause(1);   % give a little time for Collector to start up
