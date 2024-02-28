ae2874ced084169b79c4a66431e0b6918d6d7ba6
duncanpo <dpo@mathworks.com>
2024-02-28 10:15:34 -0500

    Package into a .mltbx file

--- tools/packageMatlabInterface.m
+++ tools/packageMatlabInterface.m
@@ -1,61 +1,40 @@
-% Licensed to the Apache Software Foundation (ASF) under one
-% or more contributor license agreements.  See the NOTICE file
-% distributed with this work for additional information
-% regarding copyright ownership.  The ASF licenses this file
-% to you under the Apache License, Version 2.0 (the
-% "License"); you may not use this file except in compliance
-% with the License.  You may obtain a copy of the License at
-%
-%   http://www.apache.org/licenses/LICENSE-2.0
-%
-% Unless required by applicable law or agreed to in writing,
-% software distributed under the License is distributed on an
-% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-% KIND, either express or implied.  See the License for the
-% specific language governing permissions and limitations
-% under the License.
+% packageMatlabInterface.m
+% This script packages installed files for OpenTelemetry interface and
+% package them into a single .mltbx file. The location of the installed
+% files is taken from environment variable OTEL_MATLAB_TOOLBOX_FOLDER and
+% the resulting .mltbx file location is taken from environment variable
+% OTEL_MATLAB_TOOLBOX_OUTPUT_FOLDER.
+
+% Copyright 2024 The MathWorks, Inc.
 
 toolboxFolder = string(getenv("OTEL_MATLAB_TOOLBOX_FOLDER"));
 outputFolder = string(getenv("OTEL_MATLAB_TOOLBOX_OUTPUT_FOLDER"));
-toolboxVersionRaw = string(getenv("OTEL_MATLAB_TOOLBOX_VERSION"));
+toolboxVersion = string(getenv("OTEL_MATLAB_TOOLBOX_VERSION"));
 
 % Output folder must exist.
 mkdir(outputFolder);
 
 disp("Toolbox Folder: " + toolboxFolder);
 disp("Output Folder: " + outputFolder);
-disp("Toolbox Version Raw: " + toolboxVersionRaw);
-
-% Note: This string processing heuristic may not be robust to future
-% changes in the versioning scheme.
-dotIdx = strfind(toolboxVersionRaw, ".");
-numDots = numel(dotIdx);
-if numDots >= 3
-    toolboxVersion = extractBefore(toolboxVersionRaw, dotIdx(3));
-else
-    toolboxVersion = toolboxVersionRaw;
-end
-
 disp("Toolbox Version:" + toolboxVersion);
 
 identifier = "dc2cae2f-4f43-4d2c-b6ed-f1a59f0dfcdf";
 opts = matlab.addons.toolbox.ToolboxOptions(toolboxFolder, identifier);
 opts.ToolboxName = "MATLAB Interface to OpenTelemetry";
 opts.ToolboxVersion = toolboxVersion;
-opts.AuthorName = "";
+opts.ToolboxMatlabPath = toolboxFolder;
+disp("Toolbox MATLAB Path: " + opts.ToolboxMatlabPath);
+opts.AuthorName = "MathWorks DevOps Team";
 opts.AuthorEmail = "";
 
 % Set the SupportedPlatforms
 opts.SupportedPlatforms.Win64 = true;
 opts.SupportedPlatforms.Maci64 = true;
 opts.SupportedPlatforms.Glnxa64 = true;
-%opts.SupportedPlatforms.MatlabOnline = true;
+opts.SupportedPlatforms.MatlabOnline = false;
 
-% Interface is only qualified against R2023a at the moment
-opts.MinimumMatlabRelease = "R2023a";
-opts.MaximumMatlabRelease = "R2023a";
+opts.MinimumMatlabRelease = "R2022a";
 
-%opts.OutputFile = fullfile(outputFolder, compose("otel-matlab-%s.mltbx", toolboxVersionRaw));
 opts.OutputFile = fullfile(outputFolder, "otel-matlab.mltbx");
 disp("Output File: " + opts.OutputFile);
 matlab.addons.toolbox.packageToolbox(opts);
