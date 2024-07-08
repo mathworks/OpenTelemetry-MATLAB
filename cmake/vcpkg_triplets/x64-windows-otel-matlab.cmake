4eb5d95d01a6baf9270bc594635a7ec551d57579
duncanpo <dpo@mathworks.com>
2024-07-08 11:59:20 -0400

    Define _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR preprocessor macro to work around invalid MEX file issue on Windows. Fixes #130

--- cmake/vcpkg_triplets/x64-windows-otel-matlab.cmake
+++ cmake/vcpkg_triplets/x64-windows-otel-matlab.cmake
@@ -6,4 +6,6 @@ if(${PORT} MATCHES "abseil")
 else()
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
-
+# Define a preprocessor macro _DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR to work around an invalid MEX file issue
+# See https://github.com/mathworks/OpenTelemetry-Matlab/issues/130
+set(VCPKG_CXX_FLAGS "/D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR")
