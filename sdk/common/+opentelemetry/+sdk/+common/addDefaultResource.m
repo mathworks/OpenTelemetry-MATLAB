129c89febabb919fc7f3d49157059f1f3ceedb50
duncanpo <dpo@mathworks.com>
2025-03-05 16:02:22 -0500

    Changes from code review

--- sdk/common/+opentelemetry/+sdk/+common/addDefaultResource.m
+++ sdk/common/+opentelemetry/+sdk/+common/addDefaultResource.m
@@ -1,4 +1,13 @@
 function [keys, values] = addDefaultResource(keys, values)
+% Add default resource attributes
+%    [KEYS, VALUES] = OPENTELEMETRY.SDK.COMMON.ADDDEFAULTRESOURCE(KEYS,
+%    VALUES) appends default resource attribute names and values to the
+%    input attribute names and values. The appended attribute names and
+%    values are returned as output. Default resource attributes are
+%    attributes that are always included in all telemetry data generated in
+%    this package.
+
+% Copyright 2025 The MathWorks, Inc.
 
 % MATLAB version
 v = string(version);
