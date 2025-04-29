850f726229af64839765806e1a9e4fbd500574df
duncanpo <dpo@mathworks.com>
2025-04-29 12:02:09 -0400

    install OpenSSL on Mac with gRPC

--- cmake/vcpkg_triplets/arm64-osx-otel-matlab.cmake
+++ cmake/vcpkg_triplets/arm64-osx-otel-matlab.cmake
@@ -1,6 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-if(${PORT} MATCHES "(curl|zlib)")
+if(${PORT} MATCHES "(curl|zlib|openssl)")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
     set(VCPKG_LIBRARY_LINKAGE static)
