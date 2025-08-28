47908a1982486bc3d2ebd3572511a3bd66aab6a7
duncanpo <dpo@mathworks.com>
2025-08-28 14:33:05 -0400

    fix a typo in Mac triplet files

--- cmake/vcpkg_triplets/x86_64-osx-otel-matlab.cmake
+++ cmake/vcpkg_triplets/x86_64-osx-otel-matlab.cmake
@@ -5,11 +5,11 @@ if(${PORT} MATCHES "(curl|zlib)")
 else()
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
-+# disable the script to fix rpath for curl, which makes an undesirable change to
-+# the install name from @rpath/libcurl.4.dylib to @rpath/libcurl.4.8.0.dylib
-+if(${PORT} MATCHES "curl")
-+    set(VCPKG_FIXUP_MACHO_RPATH OFF)
-+endif()
+# disable the script to fix rpath for curl, which makes an undesirable change to
+# the install name from @rpath/libcurl.4.dylib to @rpath/libcurl.4.8.0.dylib
+if(${PORT} MATCHES "curl")
+    set(VCPKG_FIXUP_MACHO_RPATH OFF)
+endif()
 
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
