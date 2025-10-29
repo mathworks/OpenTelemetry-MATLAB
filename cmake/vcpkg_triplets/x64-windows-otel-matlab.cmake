cc938d84d6c82a0dbdb1b5975445311d83d0b0de
duncanpo <dpo@mathworks.com>
2025-10-28 21:05:45 -0400

    build gRPC from source

--- cmake/vcpkg_triplets/x64-windows-otel-matlab.cmake
+++ cmake/vcpkg_triplets/x64-windows-otel-matlab.cmake
@@ -2,7 +2,7 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 # Conflict with abseil_dll.dll used by Simulink. cares.dll and re2.dll are also shipped with MATLAB. 
 # Use static libraries to avoid conflict.
-if(${PORT} MATCHES "(abseil|c-ares|re2)")
+if(${PORT} MATCHES "(protobuf|abseil|c-ares|re2)")
     set(VCPKG_LIBRARY_LINKAGE static)
 else()
     set(VCPKG_LIBRARY_LINKAGE dynamic)
